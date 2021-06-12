0c2357410c000e0f6f1f3c2ed4ede00895f1049a
RachelRicho <40514951+ICS-only@users.noreply.github.com>
2021-06-12 19:36:05 +0800

    Add files via upload

---
+++ rachelsplatform/product-service/config.py
@@ -0,0 +1,18 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    ENV = "development"
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://MSPROJ2021:rach_2021@localhost:3306/product_dev'
+
+class ProductionConfig(Config):
+    pass
